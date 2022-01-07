1cfd2a6a67385e0a406a297c86685416c6359807
leonardobernardi <lbernardi4298@gmail.com>
2022-01-07 16:29:09 +0100

    commit

--- src/main/java/org/generation/italy/controller/DepartmentController.java
+++ src/main/java/org/generation/italy/controller/DepartmentController.java
@@ -1,11 +1,13 @@
 package org.generation.italy.controller;
 
+import org.generation.italy.model.Department;
 import org.generation.italy.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +19,16 @@ public class DepartmentController {
 	
 	@GetMapping
 	public String departments(Model model) {
-		model.addAttribute("departments", repository.findAll());
+		model.addAttribute("departments", repository.findAll(Sort.by("name")));
 		return "departments";
+	}
+	
+	@GetMapping("/departments/{id}")
+	public String departmentsDetails(Model model, @PathVariable Integer id) {
+		Department d = repository.getById(id);
+		model.addAttribute("department", d);
+		model.addAttribute("degrees", d.getDegrees());
+		return "details";
 	}
 	
 }
--- src/main/java/org/generation/italy/model/Degrees.java
+++ src/main/java/org/generation/italy/model/Degrees.java
@@ -1,45 +1,32 @@
 package org.generation.italy.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="departments")
-public class Department {
-
+@Table(name="degrees")
+public class Degrees {
+	
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String name;
+	private String level;
 	private String address;
-	private String phone;
 	private String email;
 	private String website;
 	
-	@Column(name="head_of_department")
-	private String headOfDepartment;
-	
-	@OneToMany
+	@ManyToOne
 	@JoinColumn(name="department_id")
-	private List<Degrees> degrees;
-
-	public List<Degrees> getDegrees() {
-		return degrees;
-	}
-
-	public void setDegrees(List<Degrees> degrees) {
-		this.degrees = degrees;
-	}
+	private Department department;
 
 	public Integer getId() {
 		return id;
@@ -57,20 +44,20 @@ public class Department {
 		this.name = name;
 	}
 
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
 	public String getAddress() {
 		return address;
 	}
 
 	public void setAddress(String address) {
 		this.address = address;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
 	}
 
 	public String getEmail() {
@@ -89,12 +76,13 @@ public class Department {
 		this.website = website;
 	}
 
-	public String getHeadOfDepartment() {
-		return headOfDepartment;
+	public Department getDepartment() {
+		return department;
 	}
 
-	public void setHeadOfDepartment(String headOfDepartment) {
-		this.headOfDepartment = headOfDepartment;
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 	
+
 }
